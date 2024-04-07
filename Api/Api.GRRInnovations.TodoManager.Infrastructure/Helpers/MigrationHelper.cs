755eec9a2fb085558e88a0d2844c8bfbe94b8774
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2024-04-07 13:22:10 -0300

    feat: creating controllers

---
+++ Api/Api.GRRInnovations.TodoManager.Infrastructure/Helpers/MigrationHelper.cs
@@ -0,0 +1,18 @@
+﻿using Api.GRRInnovations.TodoManager.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.GRRInnovations.TodoManager.Infrastructure.Helpers
+{
+    public static class MigrationHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            Console.WriteLine("Aplicando migração CW");
+
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}

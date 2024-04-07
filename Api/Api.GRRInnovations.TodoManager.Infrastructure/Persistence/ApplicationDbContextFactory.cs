f346020c1006cc3a186f8b8614e19bd9be88eb94
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2024-04-07 13:03:42 -0300

    feat: creating dbcontext factory

---
+++ Api/Api.GRRInnovations.TodoManager.Infrastructure/Persistence/ApplicationDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Api.GRRInnovations.TodoManager.Infrastructure.Helpers;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace Api.GRRInnovations.TodoManager.Infrastructure.Persistence
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile($"appsettings.json", true)
+               .AddEnvironmentVariables()
+               .Build();
+
+            var connectionString = config.GetConnectionString("SqlConnectionString");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            var connection = string.IsNullOrEmpty(databaseUrl) ? connectionString : ConnectionHelper.BuildConnectionString(databaseUrl);
+
+            Debug.WriteLine(connectionString);
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseNpgsql(connection);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}

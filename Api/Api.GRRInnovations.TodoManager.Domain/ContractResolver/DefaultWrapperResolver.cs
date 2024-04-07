335aab377b628ec7bdc4ef89d56d82f6e2cd11ab
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2024-04-06 22:10:03 -0300

    feat: creating resolvers in the json

---
+++ Api/Api.GRRInnovations.TodoManager.Domain/ContractResolver/DefaultWrapperResolver.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Api.GRRInnovations.TodoManager.Domain.ContractResolver
+{
+    public class DefaultWrapperResolver
+    {
+        public class DefaultDataResolver : DefaultContractResolver
+        {
+            public override JsonContract ResolveContract(Type type)
+            {
+                var result = base.CreateContract(type);
+
+                return result;
+            }
+
+            public static async Task<TResult> Deserialize<TResult>(Stream stream) where TResult : new()
+            {
+                var result = default(TResult);
+
+                using (var sr = new StreamReader(stream))
+                {
+                    var json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+                    result = Deserialize<TResult>(json);
+                }
+
+                return result;
+            }
+
+            public static TResult Deserialize<TResult>(string json) where TResult : new()
+            {
+                var result = JsonConvert.DeserializeObject<TResult>(json, new JsonSerializerSettings
+                {
+                    ContractResolver = new DefaultDataResolver(),
+                    TypeNameHandling = TypeNameHandling.Auto
+                });
+
+                return result;
+            }
+        }
+    }
+}

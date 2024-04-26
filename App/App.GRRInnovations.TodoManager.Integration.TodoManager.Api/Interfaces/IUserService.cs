8fd4dfff123812d0f6717fc4e2f5285b2f858238
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2024-04-25 22:28:06 -0300

    feat: created interfaces for a other services

---
+++ App/App.GRRInnovations.TodoManager.Integration.TodoManager.Api/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.GRRInnovations.TodoManager.Integration.TodoManager.Api.Interfaces
+{
+    public interface IUserService
+    {
+    }
+}

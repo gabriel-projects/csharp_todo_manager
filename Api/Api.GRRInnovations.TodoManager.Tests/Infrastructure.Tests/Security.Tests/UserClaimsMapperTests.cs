fb9368b15312df8d948fe3085e1f24e821a480c0
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2025-04-01 00:28:42 -0300

    creating test for userclaims mapper

---
+++ Api/Api.GRRInnovations.TodoManager.Tests/Infrastructure.Tests/Security.Tests/UserClaimsMapperTests.cs
@@ -0,0 +1,30 @@
+﻿using Api.GRRInnovations.TodoManager.Infrastructure.Security;
+using System.Security.Claims;
+
+namespace Api.GRRInnovations.TodoManager.Tests.Infrastructure.Tests.Security.Tests
+{
+    public class UserClaimsMapperTests
+    {
+        [Fact]
+        public void Should_Map_Claims_With_GivenName_And_Surname()
+        {
+            //arrange
+            var userClaimsMapper = new UserClaimsMapper();
+
+            //act
+            var claims = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.GivenName, "John"),
+                new Claim(ClaimTypes.Surname, "Doe"),
+                new Claim(ClaimTypes.Name, "John Doe"),
+            }));
+            var userClaimsModel = userClaimsMapper.MapFromClaimsPrincipal(claims);
+
+            //assert
+            Assert.NotNull(userClaimsModel);
+            Assert.Equal("John", userClaimsModel.FirstName);
+            Assert.Equal("Doe", userClaimsModel.LastName);
+            Assert.Equal("John Doe", userClaimsModel.Name);
+        }
+    }
+}

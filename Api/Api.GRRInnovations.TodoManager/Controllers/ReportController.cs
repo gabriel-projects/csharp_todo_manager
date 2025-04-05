926016887f14f3a16552bfdbc8bf8357b6a8d0ef
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2025-04-05 01:58:10 -0300

    todo para gerar um pdf

--- Api/Api.GRRInnovations.TodoManager/Controllers/ReportController.cs
+++ Api/Api.GRRInnovations.TodoManager/Controllers/ReportController.cs
@@ -9,5 +9,18 @@ namespace Api.GRRInnovations.TodoManager.Controllers
     [ApiController]
     public class ReportController : ControllerBase
     {
+        [HttpGet("report/{userId}")]
+        public IActionResult GetReport(Guid userId)
+        {
+            //todo: gerar um pdf ou um excel com os dados do usuário
+            var report = new
+            {
+                UserId = userId,
+                ReportDate = DateTime.UtcNow,
+                TasksCompleted = 5,
+                TasksPending = 2
+            };
+            return Ok(report);
+        }
     }
 }
